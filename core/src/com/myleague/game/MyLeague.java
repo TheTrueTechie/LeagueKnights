d95cd392de791db75948ca605b6fc1fe7930d52b
J.D.S. Solutions <league@WTS13.attlocal.net>
2018-03-02 18:34:16 -0800

    Migrated Project to Repo

---
+++ core/src/com/myleague/game/MyLeague.java
@@ -0,0 +1,42 @@
+package com.myleague.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class MyLeague extends ApplicationAdapter {
+	SpriteBatch batch;
+	BitmapFont font;
+	Texture img;
+	World world = new World();
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		font = new BitmapFont();
+		font.setColor(Color.FIREBRICK);
+		img = new Texture("badlogic.jpg");
+		world.create();
+	}
+
+	@Override
+	public void render () {
+		Gdx.graphics.setTitle("FPS : " + Gdx.graphics.getFramesPerSecond());
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		world.render(batch, img);
+		font.draw(batch, "LEAGUE KNIGHTS", 300, 400);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+}
