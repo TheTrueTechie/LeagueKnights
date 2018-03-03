1e77bf6fe8ff8b0d4604cbf2e40d2f2d8b4f1b10
J.D.S. Solutions <zaelix@gmail.com>
2018-03-02 23:21:09 -0800

    Freed the desktop build from external dependencies

---
+++ desktop/src/com/leagueknights/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.leagueknights.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.leagueknights.game.LeagueKnights;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LeagueKnights(), config);
+	}
+}
