d79c387568bdf4c914acd8bd973bb52545431a99
Michael Johnson <zaelix@gmail.com>
2018-03-02 23:50:55 -0800

    Added sprite flipping for leftward movement

--- core/src/com/leagueknights/game/Player.java
+++ core/src/com/leagueknights/game/Player.java
@@ -19,7 +19,8 @@ public class Player {
 
 	int x = 0;
 	int y = 20;
-	String anim = "walk";
+	String anim = "walk_left";
+	
 
 	public void create() {
 		knightIdleAtlas = new TextureAtlas(Gdx.files.internal("spritesheets/knight_idle.atlas"));
@@ -33,7 +34,7 @@ public class Player {
 	public void render(SpriteBatch batch) {
 		elapsedTime += Gdx.graphics.getDeltaTime();
 
-		batch.draw(getAnimation(), x, y);
+		batch.draw(getAnimation(), x, y, 128, 128);
 		controls();
 	}
 
@@ -42,11 +43,17 @@ public class Player {
 		TextureRegion ret;
 		if (anim.equals("attack")) {
 			ret = knightSlashAnim.getKeyFrame(elapsedTime, true);
-		}
-		else if (anim.equals("walk")) {
+		} else if (anim.equals("walk_right")) {
 			ret = knightWalkAnim.getKeyFrame(elapsedTime, true);
-		}
-		else if (anim.equals("idle")) {
+			if(ret.isFlipX()) {
+				ret.flip(true, false);
+			}
+		} else if (anim.equals("walk_left")) {
+			ret = knightWalkAnim.getKeyFrame(elapsedTime, true);
+			if(!ret.isFlipX()) {
+				ret.flip(true, false);
+			}
+		} else if (anim.equals("idle")) {
 			ret = knightIdleAnim.getKeyFrame(elapsedTime, true);
 		} else {
 			ret = knightWalkAnim.getKeyFrame(elapsedTime, true);
@@ -55,20 +62,20 @@ public class Player {
 	}
 
 	public void controls() {
-		if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-			anim = "walk";
+		if (Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
+			anim = "attack";
+		} else if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+			anim = "walk_left";
 			if (Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
 				x -= 2;
 			else
 				x -= 1;
 		} else if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-			anim = "walk";
+			anim = "walk_right";
 			if (Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
 				x += 2;
 			else
 				x += 1;
-		} else if (Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
-			anim = "attack";
 		} else {
 			anim = "idle";
 		}
