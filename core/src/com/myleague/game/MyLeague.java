78402f9f45ce585da4d4bafab6c2e58d2155b9fe
J.D.S. Solutions <league@WTS5.attlocal.net>
2018-03-04 14:00:25 -0800

    Camera, mouse control
    Now uses camera movement for the view, and the mouse controls
    directionality based on screen-space mouse position.

--- core/src/com/myleague/game/MyLeague.java
+++ core/src/com/myleague/game/MyLeague.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -28,6 +29,7 @@ public class MyLeague extends ApplicationAdapter {
 		Gdx.graphics.setTitle("FPS : " + Gdx.graphics.getFramesPerSecond());
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
 		batch.begin();
 		world.render(batch);
 		//font.draw(batch, "LEAGUE KNIGHTS", 300, 400);
--- core/src/com/myleague/game/Player.java
+++ core/src/com/myleague/game/Player.java
@@ -3,13 +3,16 @@ package com.myleague.game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector3;
 
 public class Player {
+	private OrthographicCamera camera;
 	BitmapFont font;
 	TextureAtlas knightIdleAtlas;
 	TextureAtlas knightWalkAtlas;
@@ -23,8 +26,9 @@ public class Player {
 	float health = 100;
 	boolean isFacingRight = true;
 	int attackTimer = 0;
+	int walkSpeed = 1;
 	int x = 0;
-	int y = 20;
+	int y = 0;
 	String anim = "idle";
 
 	public void create() {
@@ -36,18 +40,24 @@ public class Player {
 		knightWalkAnim = new Animation<TextureRegion>(1 / 8f, knightWalkAtlas.getRegions());
 		knightSlashAnim = new Animation<TextureRegion>(1 / 10f, knightSlashAtlas.getRegions());
 		knightDeathAnim = new Animation<TextureRegion>(1 / 9f, knightDeathAtlas.getRegions());
+		camera = new OrthographicCamera(1280, 720);
+		camera.translate(80, 300);
+		camera.update();
 
+		
 		font = new BitmapFont();
 		font.setColor(Color.RED);
 	}
 
 	public void render(SpriteBatch batch) {
 		elapsedTime += Gdx.graphics.getDeltaTime();
-
-		controls();
+		controlsMKB();
+		batch.setProjectionMatrix(camera.combined);
 		batch.draw(getAnimation(), x, y, 128, 128);
-		font.draw(batch, "HEALTH: " + health, 10, 590);
+		font.draw(batch, "HEALTH: " + health, x, 590);
 		attackTimer--;
+		
+		System.out.println("Player: (" + x + "," + y +"), MouseX: " + Gdx.input.getX() );
 	}
 
 	public TextureRegion getAnimation() {
@@ -55,11 +65,7 @@ public class Player {
 		TextureRegion ret;
 		if (anim.equals("attack")) {
 			ret = knightSlashAnim.getKeyFrame(elapsedTime, false);
-		} else if (anim.equals("walk_right")) {
-			isFacingRight = true;
-			ret = knightWalkAnim.getKeyFrame(elapsedTime, true);
-		} else if (anim.equals("walk_left")) {
-			isFacingRight = false;
+		} else if (anim.equals("walk")) {
 			ret = knightWalkAnim.getKeyFrame(elapsedTime, true);
 		} else if (anim.equals("death")) {
 			ret = knightDeathAnim.getKeyFrame(elapsedTime, true);
@@ -75,7 +81,7 @@ public class Player {
 		return ret;
 	}
 
-	public void controls() {
+	public void controlsKB() {
 		if (Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
 			anim = "attack";
 			attackTimer = 50;
@@ -83,13 +89,15 @@ public class Player {
 		}
 		if (attackTimer < 0) {
 			if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-				anim = "walk_left";
+				anim = "walk";
+				isFacingRight = false;
 				if (Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
 					x -= 2;
 				else
 					x -= 1;
 			} else if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-				anim = "walk_right";
+				anim = "walk";
+				isFacingRight = true;
 				if (Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
 					x += 2;
 				else
@@ -97,6 +105,51 @@ public class Player {
 			} else {
 				anim = "idle";
 			}
+		}
+	}
+	
+	public void controlsMKB() {
+		if (Gdx.input.isButtonPressed(Input.Buttons.LEFT)) {
+			anim = "attack";
+			attackTimer = 50;
+			elapsedTime = 0;
+		}
+		if (attackTimer < 0) {
+			int move = 0;
+			if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+				anim = "walk";
+				if (Gdx.input.isKeyPressed(Input.Keys.SHIFT_LEFT)) 
+					move = walkSpeed*-2;
+				else
+					move = -walkSpeed;
+			} else if (Gdx.input.isKeyPressed(Input.Keys.D)) {
+				anim = "walk";
+				if (Gdx.input.isKeyPressed(Input.Keys.SHIFT_LEFT))
+					move = walkSpeed*2;
+				else
+					move = walkSpeed;
+			} else {
+				anim = "idle";
+			}
+			
+			x += move;
+			camera.translate(move, 0);
+		}
+		
+		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+			camera.translate(2, 0);
+			System.out.println(camera.position);
+		}
+		else if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+			camera.translate(-2, 0);
+		}
+		camera.update();
+		
+		if(Gdx.input.getX() > 625) {
+			isFacingRight = true;
+		}
+		else {
+			isFacingRight = false;
 		}
 	}
 }
