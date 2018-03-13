757b7fe229ea6af678c9b92f436b24d69b41da4f
Michael Johnson <zaelix@gmail.com>
2018-03-13 12:54:35 -0700

    Block Animation, shape drawing to camera space

--- core/src/com/myleague/game/Player.java
+++ core/src/com/myleague/game/Player.java
@@ -19,11 +19,13 @@ public class Player {
 	TextureAtlas knightIdleAtlas;
 	TextureAtlas knightWalkAtlas;
 	TextureAtlas knightSlashAtlas;
+	TextureAtlas knightBlockAtlas;
 	TextureAtlas knightDeathAtlas;
 	Animation<TextureRegion> knightIdleAnim;
 	Animation<TextureRegion> knightWalkAnim;
 	Animation<TextureRegion> knightRunAnim;
 	Animation<TextureRegion> knightSlashAnim;
+	Animation<TextureRegion> knightBlockAnim;
 	Animation<TextureRegion> knightDeathAnim;
 	float elapsedTime = 0;
 	float health = 100;
@@ -39,11 +41,13 @@ public class Player {
 		knightIdleAtlas = new TextureAtlas(Gdx.files.internal("spritesheets/knight_idle.atlas"));
 		knightWalkAtlas = new TextureAtlas(Gdx.files.internal("spritesheets/knight_walk.atlas"));
 		knightSlashAtlas = new TextureAtlas(Gdx.files.internal("spritesheets/knight_slash.atlas"));
+		knightBlockAtlas = new TextureAtlas(Gdx.files.internal("spritesheets/knight_block.atlas"));
 		knightDeathAtlas = new TextureAtlas(Gdx.files.internal("spritesheets/knight_death.atlas"));
 		knightIdleAnim = new Animation<TextureRegion>(1 / 4f, knightIdleAtlas.getRegions());
 		knightWalkAnim = new Animation<TextureRegion>(1 / 8f, knightWalkAtlas.getRegions());
 		knightRunAnim = new Animation<TextureRegion>(1 / 4f, knightWalkAtlas.getRegions());
 		knightSlashAnim = new Animation<TextureRegion>(1 / 10f, knightSlashAtlas.getRegions());
+		knightBlockAnim = new Animation<TextureRegion>(1 / 7f, knightBlockAtlas.getRegions());
 		knightDeathAnim = new Animation<TextureRegion>(1 / 9f, knightDeathAtlas.getRegions());
 		camera = new OrthographicCamera(1280, 720);
 		camera.translate(80, 300);
@@ -51,7 +55,6 @@ public class Player {
 
 		sr = new ShapeRenderer();
 		sr.setProjectionMatrix(camera.combined);
-		sr.begin(ShapeType.Line);
 
 		font = new BitmapFont();
 		font.setColor(Color.RED);
@@ -59,13 +62,15 @@ public class Player {
 
 	public void render(SpriteBatch batch) {
 		elapsedTime += Gdx.graphics.getDeltaTime();
-		controlsMKB();
+		if(!anim.equals("death")) {
+			controlsMKB();
+		}
 		batch.setProjectionMatrix(camera.combined);
 		batch.draw(getAnimation(), x, y, 128, 128);
 		font.draw(batch, "HEALTH: " + health, x + 25, 500);
 		attackTimer--;
 
-		drawShapes(batch);
+		// drawShapes(batch);
 		// System.out.println("Player: (" + x + "," + y +"), MouseX: " +
 		// Gdx.input.getX() );
 	}
@@ -75,12 +80,14 @@ public class Player {
 		TextureRegion ret;
 		if (anim.equals("attack")) {
 			ret = knightSlashAnim.getKeyFrame(elapsedTime, false);
+		} else if (anim.equals("block")) {
+			ret = knightBlockAnim.getKeyFrame(elapsedTime, false);
 		} else if (anim.equals("walk")) {
 			ret = knightWalkAnim.getKeyFrame(elapsedTime, true);
 		} else if (anim.equals("run")) {
 			ret = knightRunAnim.getKeyFrame(elapsedTime, true);
 		} else if (anim.equals("death")) {
-			ret = knightDeathAnim.getKeyFrame(elapsedTime, true);
+			ret = knightDeathAnim.getKeyFrame(elapsedTime, false);
 		} else {
 			ret = knightIdleAnim.getKeyFrame(elapsedTime, true);
 		}
@@ -93,15 +100,20 @@ public class Player {
 		return ret;
 	}
 
+	/**
+	 * Demonstrates the various shaperender capabilities
+	 * @param batch
+	 */
 	public void drawShapes(SpriteBatch batch) {
 		batch.end();
 		// camera.update();
 
-		// sr.setColor(1, 1, 0, 1);
-		sr.line(x, y, x + 100, y + 100);
+		sr.begin(ShapeType.Line);
+		sr.setColor(1, 1, 0, 1);
+		sr.line(x, y, x + 1000, y + 1000);
 		sr.rect(x, y, 100, 100);
-		// sr.circle(x, y, 100);
-		// sr.end();
+		sr.circle(Gdx.graphics.getWidth()/8, Gdx.graphics.getWidth()/3, 100);
+		sr.end();
 		batch.begin();
 	}
 
@@ -142,6 +154,10 @@ public class Player {
 			attackTimer = 50;
 			elapsedTime = 0;
 			SoundHandler.playSlash();
+		}if (Gdx.input.isButtonPressed(Input.Buttons.RIGHT)) {
+			anim = "block";
+			attackTimer = 50;
+			elapsedTime = 0;
 		}
 		if (attackTimer < 0) {
 			int move = 0;
@@ -175,6 +191,12 @@ public class Player {
 		} else if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
 			camera.translate(-2, 0);
 		}
+		else if(Gdx.input.isKeyJustPressed(Input.Keys.UP)) {
+			takeDamage(-5);
+		}
+		else if(Gdx.input.isKeyJustPressed(Input.Keys.DOWN)) {
+			takeDamage(5);
+		}
 		camera.update();
 
 		if (Gdx.input.getX() > Gdx.graphics.getWidth()/2) {
@@ -184,6 +206,14 @@ public class Player {
 		}
 	}
 
+	public void takeDamage(int dmg) {
+		this.health -= dmg;
+		if(health <=0) {
+			this.anim= "death";
+			elapsedTime = 0;
+		}
+	}
+	
 	public int getX() {
 		return this.x;
 	}
